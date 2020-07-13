4764143a1451c850bdc7e25e222b694cfc3d042f
X͜͡๖ۣۜBEAN々NoReal <66841000+irfanfebrian13@users.noreply.github.com>
2020-07-13 22:06:28 +0700

    Add source & output

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -17,15 +17,19 @@ async def corona(event):
     covid = Covid()
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`😔Total Cases     : {country_data['confirmed']}`\n"
-        output_text += f"`😔Active Cases    : {country_data['active']}`\n"
+        output_text  = f"`😕New Cases       : {country_data['new_cases']}`\n"
+        output_text += f"`😭New Deaths      : {country_data['new_deaths']}`\n\n"
+        output_text += f"`😔Total Cases     : {country_data['confirmed']}`\n"
+        output_text += f"`😔Active Cases    : {country_data['active']}`\n\n"
         output_text += f"`😭Total Deaths    : {country_data['deaths']}`\n"
-        output_text += f"`😍Total Recovered : {country_data['recovered']}`\n"
+        output_text += f"`😍Total Recovered : {country_data['recovered']}`\n\n"
+        output_text += f"`😥Critical cases  : {country_data['critical']}`\n"
+        output_text += f"`💉Total Tests     : {country_data['total_tests']}`\n\n"
         output_text += (
             "`Last update : "
             f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
         )
-        output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
+        output_text += f""Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
     else:
         output_text = "No information yet about this country!"
     await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
