0973189a11f2411c16235984326bb8f6cad6380e
X͜͡๖ۣۜBEAN々NoReal <66841000+irfanfebrian13@users.noreply.github.com>
2020-07-15 01:33:36 +0700

    change version ubot

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -129,7 +129,7 @@ COUNTRY = str(os.environ.get("COUNTRY", ""))
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
 
 # Version of ProjectBish
-USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "3.4"
+USERBOT_VERSION = os.environ.get("USERBOT_VERSION") or "1.beta.6"
 
 # Clean Welcome
 CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
