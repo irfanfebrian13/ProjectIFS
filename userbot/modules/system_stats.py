88c42eda7d29290030530a8f8d71d8f099a3b482
X͜͡๖ۣۜBEAN々NoReal <66841000+irfanfebrian13@users.noreply.github.com>
2020-07-17 21:17:40 +0700

    Beautify alive

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -131,15 +131,15 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     logo = ALIVE_LOGO
-    output = (f"╔─━━━━━━━**INFO**━━━━━━━━─╗\n"
-             f" ┣--❏`ProjectBish Is Running` ❏ --\n"
-             f" ╚─━━━━━━━**INFO**━━━━━━━━─╝\n\n"
+    output = (f"╔─━━━━━━━━━**INFO**━━━━━━━━━━━─╗\n"
+             f"┣-❏**•ProjectBish Is Running•**-❏ \n"
+             f"╚─━━━━━━━━━**INFO**━━━━━━━━━━━─╝\n\n"
              f"┏━━━━━━━━━━━━━━━━━━━━━━━━\n"
-             f"┣[ 👤 `User        :` {DEFAULTUSER}\n"
-             f"┣[ 🐛 `Running on  : {UPSTREAM_REPO_BRANCH} `\n"
-             f"┣[ 🐍 `Python      : v{python_version()}`\n"
-             f"┣[ ⚙️ `Telethon    : v{version.__version__}`\n"
-             f"┣[ 🧸 `Version     : v{USERBOT_VERSION} `\n"
+             f"┣[ 🗣️ `User        :` {DEFAULTUSER}\n"
+             f"┣[ 🧿 `Running on  : {UPSTREAM_REPO_BRANCH} `\n"
+             f"┣[ ☠️ `Python      : v{python_version()}`\n"
+             f"┣[ 🔧 `Telethon    : v{version.__version__}`\n"
+             f"┣[ ♈ `Version     : v{USERBOT_VERSION} `\n"
              f"┗━━━━━━━━━━━━━━━━━━━━━━━━\n")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
