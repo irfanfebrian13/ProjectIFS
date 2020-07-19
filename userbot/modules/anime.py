e43cb8b6eaf7975aace4e571e5f93b0c23af5085
Angga <angga@linuxmail.org>
2020-07-20 04:59:24 +0700

    anime: add anilist module
    Change-Id: I42b1e08831647a29a8c65667fa9114ce611fcfc1
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/anime.py
+++ userbot/modules/anime.py
@@ -7,6 +7,7 @@
 import asyncio
 import html
 import json
+import re
 import textwrap
 from io import BytesIO, StringIO
 from urllib.parse import quote as urlencode
@@ -20,8 +21,11 @@ from html_telegraph_poster import TelegraphPoster
 from jikanpy import Jikan
 from jikanpy.exceptions import APIException
 from telethon.errors.rpcerrorlist import FilePartsInvalidError
-from telethon.tl.types import (DocumentAttributeAnimated,
-                               DocumentAttributeFilename, MessageMediaDocument)
+from telethon.tl.types import (
+    DocumentAttributeAnimated,
+    DocumentAttributeFilename,
+    MessageMediaDocument,
+)
 from telethon.utils import is_image, is_video
 
 from userbot import CMD_HELP
@@ -192,6 +196,83 @@ def replace_text(text):
             "").replace(
                 "\\",
         "")
+
+
+async def callAPI(search_str):
+    query = """
+    query ($id: Int,$search: String) {
+      Media (id: $id, type: ANIME,search: $search) {
+        id
+        title {
+          romaji
+          english
+        }
+        description (asHtml: false)
+        startDate{
+            year
+          }
+          episodes
+          chapters
+          volumes
+          season
+          type
+          format
+          status
+          duration
+          averageScore
+          genres
+          bannerImage
+      }
+    }
+    """
+    variables = {"search": search_str}
+    url = "https://graphql.anilist.co"
+    response = requests.post(
+        url,
+        json={
+            "query": query,
+            "variables": variables})
+    return response.text
+
+
+async def formatJSON(outData):
+    msg = ""
+    jsonData = json.loads(outData)
+    res = list(jsonData.keys())
+    if "errors" in res:
+        msg += f"**Error** : `{jsonData['errors'][0]['message']}`"
+        return msg
+    else:
+        jsonData = jsonData["data"]["Media"]
+        if "bannerImage" in jsonData.keys():
+            msg += f"[〽️]({jsonData['bannerImage']})"
+        else:
+            msg += "〽️"
+        title = jsonData["title"]["romaji"]
+        link = f"https://anilist.co/anime/{jsonData['id']}"
+        msg += f"[{title}]({link})"
+        msg += f"\n\n**Type** : {jsonData['format']}"
+        msg += f"\n**Genres** : "
+        for g in jsonData["genres"]:
+            msg += g + " "
+        msg += f"\n**Status** : {jsonData['status']}"
+        msg += f"\n**Episode** : {jsonData['episodes']}"
+        msg += f"\n**Year** : {jsonData['startDate']['year']}"
+        msg += f"\n**Score** : {jsonData['averageScore']}"
+        msg += f"\n**Duration** : {jsonData['duration']} min\n\n"
+        cat = f"{jsonData['description']}"
+        msg += " __" + re.sub("<br>", "\n", cat) + "__"
+        return msg
+
+
+@register(outgoing=True, pattern=r"^\.anilist ?(.*)")
+async def anilist(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    result = await callAPI(input_str)
+    msg = await formatJSON(result)
+    await event.edit(msg, link_preview=True)
 
 
 @register(outgoing=True, pattern=r"^\.anime ?(.*)")
@@ -660,7 +741,9 @@ def is_gif(file):
 
 CMD_HELP.update({
     "anime":
-    ">`.anime` **<anime>**"
+    ">`.anilist` **<anime>**"
+    "\nUsage: Get anime information from anilist."
+    "\n\n>`.anime` **<anime>**"
     "\nUsage: Returns with Anime information."
     "\n\n>`.manga` **<manga name>**"
     "\nUsage: Returns with the Manga information."
