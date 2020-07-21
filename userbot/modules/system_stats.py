9a95b87d8f8fea9710f9fec723d977ad0ab7b5a4
Angga <angga@linuxmail.org>
2020-07-21 23:33:26 +0700

    system_stats: alive: add bot uptime

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -6,6 +6,7 @@
 """ Userbot module for getting information about the server. """
 import platform
 import sys
+import time
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from datetime import datetime
@@ -15,12 +16,40 @@ from shutil import which
 import psutil
 from telethon import __version__, version
 
-from userbot import bot, CMD_HELP, ALIVE_NAME, UPSTREAM_REPO_BRANCH, ALIVE_LOGO
+from userbot import bot, CMD_HELP, ALIVE_NAME, UPSTREAM_REPO_BRANCH, ALIVE_LOGO, StartTime
 from userbot.events import register
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
+
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
 
 
 @register(outgoing=True, pattern=r"^\.spc")
@@ -186,10 +215,12 @@ async def pipcheck(pip):
 
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running.  """
+    """ For .on command, check if the bot is running.  """
+    uptime = await get_readable_time((time.time() - StartTime))
     logo = ALIVE_LOGO
     output = (f"╔─━━━━━━━━━**INFO**━━━━━━━─╗\n"
               f"┣-❏**•ProjectBish Is Running•**-❏ \n"
+              f"      -❏`{uptime}`❏-\n"
               f"╚─━━━━━━━━━**INFO**━━━━━━━─╝\n\n"
               f"┏━━━━━━━━━━━━━━━━━━━━━━━━\n"
               f"┣[ 🗣️ `User        :` {DEFAULTUSER}\n"
