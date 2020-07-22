e0cfd00133ea5765211aac9a7db4667383601bab
X͜͡๖ۣۜBEAN々NoReal <66841000+irfanfebrian13@users.noreply.github.com>
2020-07-22 10:42:18 +0700

    add WEATHER_DEFLANG

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -113,6 +113,7 @@ GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
 # OpenWeatherMap API Key
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
 WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY", None)
+WEATHER_DEFLANG = os.environ.get("WEATHER_DEFLANG") or None
 
 # Anti Spambot Config
 ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
