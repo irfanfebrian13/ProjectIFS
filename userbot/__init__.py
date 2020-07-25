617aca2f6ffe546c2d7f953d3c26a4f380401052
Angga <angga@linuxmail.org>
2020-07-26 01:41:11 +0700

    fix google chrome stuff
    Since this commit https://github.com/aidilaryanto/ProjectDils/commit/2095845e0e3a8f10a17b9539ffe079bebe96a2ab
    Need change google chrome config stuff
    also hardcore chrome config to __init__
    
    Change-Id: I49b84a15d395ded6e065f3e9797ada792b7aec01
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: irfanfebrian13 <irfanifebrians13@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -108,8 +108,10 @@ OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
 REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
 
 # Chrome Driver and Headless Google Chrome Binaries
-CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
-GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+CHROME_DRIVER = (os.environ.get("CHROME_DRIVER")
+                 or "/usr/bin/google-chrome")
+GOOGLE_CHROME_BIN = (os.environ.get("GOOGLE_CHROME_BIN")
+                     or "/usr/bin/chromedriver")
 
 # OpenWeatherMap API Key
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
