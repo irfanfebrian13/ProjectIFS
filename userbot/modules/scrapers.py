95d679af3ef6c9553162f837dbdd6efb250cc5f7
irfanfebrian13 <irfanifebrians13@gmail.com>
2020-07-30 16:56:56 +0700

    scrapers: remove YOUTUBE_API_KEY
    Signed-off-by: irfanfebrian13 <irfanifebrians13@gmail.com>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -44,8 +44,7 @@ from userbot import (
     CMD_HELP,
     IMG_LIMIT,
     TEMP_DOWNLOAD_DIRECTORY,
-    WOLFRAM_ID,
-    YOUTUBE_API_KEY,
+    WOLFRAM_ID,   
 )
 from userbot.events import register
 from telethon.tl.types import DocumentAttributeAudio
