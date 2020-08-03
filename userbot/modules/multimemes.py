2a7b934924431c71a5aaf3b3bf1a210a401b1480
irfanfebrian13 <irfanifebrians13@gmail.com>
2020-08-04 01:31:02 +0700

    sangmata: fix derp
    Signed-off-by: irfanfebrian13 <irfanifebrians13@gmail.com>

--- userbot/modules/multimemes.py
+++ userbot/modules/multimemes.py
@@ -431,26 +431,37 @@ async def lastname(steal):
         await steal.edit("```Reply to actual users message.```")
         return
     await steal.edit("```Sit tight while I steal some data from NASA```")
-    async with bot.conversation(chat) as conv:
-        try:
-            msg = await conv.send_message(id)
-            r = await conv.get_response()
-            response = await conv.get_response()
-        except YouBlockedUserError:
-            await steal.reply("```Please unblock @sangmatainfo_bot and try again```")
-            return
-        if response.text.startswith("No records"):
-            await steal.edit("```No records found for this user```")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await steal.reply("```Please unblock @sangmatainfo_bot and try again```")
+                return
+            if r.text.startswith("Name"):
+                respond = await conv.get_response()
+                await steal.edit(f"{r.message}")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
+                )
+                return
+            if response.text.startswith(
+                    "No records") or r.text.startswith("No records"):
+                await steal.edit("```No records found for this user```")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id]
+                )
+                return
+            else:
+                respond = await conv.get_response()
+                await steal.edit(f"{response.message}")
             await steal.client.delete_messages(
-                conv.chat_id,
-                [msg.id, r.id, response.id])
-            return
-        else:
-            respond = await conv.get_response()
-            await steal.edit(f"{response.message}")
-        await steal.client.delete_messages(
-            conv.chat_id,
-            [msg.id, r.id, response.id, respond.id])
+                conv.chat_id, [msg.id, r.id, response.id, respond.id]
+            )
+    except TimeoutError:
+        return await event.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
 
 
 @register(outgoing=True, pattern=r"^\.waifu(?: |$)(.*)")
