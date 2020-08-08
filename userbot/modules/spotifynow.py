f7147fafed5f3c41df99c1d492cdd31315e13146
KenHV <kenhv@null.net>
2020-08-08 12:24:03 +0530

    spotifynow: add direct link in caption
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>
    Signed-off-by: irfanfebrian13 <irfanifebrians13@gmail.com>

--- userbot/modules/spotifynow.py
+++ userbot/modules/spotifynow.py
@@ -13,39 +13,48 @@ async def _(event):
         return
     chat = "@SpotifyNowBot"
     await event.edit("`Processing...`")
-    async with event.client.conversation(chat) as conv:
-        now = "/now"
-        try:
-            msg = await conv.send_message(now)
-            response = await conv.get_response()
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.reply("`Please unblock` @SpotifyNowBot`...`")
-            return
-        if response.text.startswith("You're"):
-            await event.edit("`You're not listening to anything on Spotify at the moment`")
-            await event.client.delete_messages(conv.chat_id,
-                                               [msg.id, response.id])
-            return
-        if response.text.startswith("Ads."):
-            await event.edit("`You're listening to those annoying ads.`")
-            await event.client.delete_messages(conv.chat_id,
-                                               [msg.id, response.id])
-            return
-        else:
-            downloaded_file_name = await event.client.download_media(
-                response.media,
-                TEMP_DOWNLOAD_DIRECTORY
-            )
-            await event.client.send_file(
-                event.chat_id,
-                downloaded_file_name,
-                force_document=False,
-            )
-            await event.client.delete_messages(conv.chat_id,
-                                               [msg.id, response.id])
-    await event.delete()
-    return os.remove(downloaded_file_name)
+    try:
+        async with event.client.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(now)
+                response = await conv.get_response()
+                """don't spam notif"""
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.reply("`Please unblock` @SpotifyNowBot`...`")
+                return
+            if response.text.startswith("You're"):
+                await event.edit("`You're not listening to anything on Spotify at the moment`")
+                await event.client.delete_messages(
+                    conv.chat_id, [msg.id, response.id]
+                )
+                return
+            if response.text.startswith("Ads."):
+                await event.edit("`You're listening to those annoying ads.`")
+                await event.client.delete_messages(
+                    conv.chat_id, [msg.id, response.id]
+                )
+                return
+            else:
+                downloaded_file_name = await event.client.download_media(
+                    response.media,
+                    TEMP_DOWNLOAD_DIRECTORY
+                )
+                link = response.reply_markup.rows[0].buttons[0].url
+                await event.client.send_file(
+                    event.chat_id,
+                    downloaded_file_name,
+                    force_document=False,
+                    caption=f"[Play on Spotify]({link})"
+                )
+                """cleanup chat after completed"""
+                await event.client.delete_messages(
+                    conv.chat_id, [msg.id, response.id]
+                )
+        await event.delete()
+        return os.remove(downloaded_file_name)
+    except TimeoutError:
+        return await event.edit("`Error: `@SpotifyNowBot` is not responding!.`")
 
 
 CMD_HELP.update({
